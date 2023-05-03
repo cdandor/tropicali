6c75b37da0ba2421a22be90b4386de31f645fc72
cdandor <clintonandor@gmail.com>
2023-05-03 13:38:10 -0700

    optimized version of the landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,49 @@
-var gulp = require("gulp")
+const { series, watch, src, dest } = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+const cleanCSS = require("gulp-clean-css")
+const sourcemaps = require("gulp-sourcemaps")
+const browserSync = require("browser-sync").create()
+const imagemin = require("gulp-imagemin")
 
-gulp.task("default", function () {
-  // place code for your default task here
-})
+function compileSass() {
+  return src("src/css/styles.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist"))
+    .pipe(browserSync.stream())
+}
+
+function copyHtml() {
+  return src("src/*.html").pipe(dest("dist"))
+}
+
+function copyFonts() {
+  return src("src/fonts/*").pipe(dest("dist/fonts"))
+}
+
+function copyImages() {
+  return src("src/img/*").pipe(imagemin()).pipe(dest("dist/img"))
+}
+
+function startWatch() {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+    },
+  })
+
+  watch("src/*.html", copyHtml).on("change", browserSync.reload)
+  watch("src/css/styles.scss", compileSass)
+  watch("src/fonts/*", copyFonts)
+  watch("src/img/*", copyImages)
+}
+
+exports.default = series(
+  copyHtml,
+  compileSass,
+  copyFonts,
+  copyImages,
+  startWatch
+)
